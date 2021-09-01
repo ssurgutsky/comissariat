93b59bc8c5ca2dad764e46d4b8eb8584cebe69a5
ssurgutsky <ssurgutsky@gmail.com>
2021-09-01 13:17:46 +0700

    - upd from Taxoman

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,15 +1,26 @@
 /* eslint no-eval: 0 */
 
 export default {
-  GAME_VERSION: 21,
-  INDEXEDDB_VERSION: 6,
-  INDEXEDDB_STORE_NAME: 'store_voenkomat',
+  // Increase this on every update to reload assets for new on startup
+  GAME_VERSION: 25,
+  // Release = true
   CACHE_ENABLED: true,
+
   GAME_VERSION_VAR_NAME: 'game_version',
   GAME_UPGRADE_POINT: 'GAME_UPGRADEPOINT',
   GAME_SAVE_POINT: 'GAME_SAVEPOINT',
   // Goto this mark if not found player's one on LOAD button pressing
-  DEFAULT_LOAD_MARK_NAME: 'START',
+  DEFAULT_LOAD_MARK_NAME: 'main_menu',
+  // Just a name of Index DB for this game
+  INDEXEDDB_STORE_NAME: 'store_voenkomat',
+  // Google Play IAP
   GP_PRODUCT1_NAME: 'com.skystairgames.voenkomat.coin1',
-  GP_PRODUCT2_NAME: 'com.skystairgames.voenkomat.episodes'
+  GP_PRODUCT2_NAME: 'com.skystairgames.voenkomat.episodes',
+  // You can enable some buttons by default in the regular game
+  ENABLE_SAVE_BUTTON: true,
+  ENABLE_LOAD_BUTTON: true,
+  ENABLE_SKIP_BUTTON: false,
+  ENABLE_EPISODES_BUTTON: false,
+  ENABLE_BACK_BUTTON: false,
+  ENABLE_DONATE_BUTTON: true
 }
